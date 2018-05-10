d47af1ece7d6284ef82182a519e6aa4440553a82
Matías Medrano Gamonal <matiasmedrano94@gmail.com>
2018-05-10 01:09:54 -0300

    Fibonacci Rest service ready

---
+++ FibonacciHttp.py
@@ -0,0 +1,22 @@
+from flask import Flask
+import json
+import mimerender
+from Fibonacci import Fibonacci
+
+mimerender = mimerender.FlaskMimeRender()
+
+render_json = lambda **args: json.dumps(args)
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/<number>')
+@mimerender(
+    default = 'json',
+    json = render_json
+)
+def showMessage(number='10'):
+    return {'n':Fibonacci(int(number)),'n-1':Fibonacci(int(number)-1)}
+
+if __name__ == "__main__":
+    app.run(port=8080)
